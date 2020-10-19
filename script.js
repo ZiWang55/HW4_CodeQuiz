2a3616956bb727b772deb3f81854b294222d5d12
Zi Wang <m@ms-macbook-air.lan>
2020-10-19 12:44:25 -0500

    added showResults function into js and confirmed that it works with browser testing

--- script.js
+++ script.js
@@ -35,7 +35,39 @@ const myQuestions = [
 ];
 
 function buildQuiz() {}
-function showResults() {}
+// showing quiz results
+function showResults() {
+
+  // gethering answers
+  const answerContainers = quizContainer.querySelectorAll('.answers');
+  // keep track of user answers
+  let numCorrect = 0;
+
+  // for each question
+  myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+    // find selected answer
+    const answerContainer = answerContainers[questionNumber];
+    const selector = `input[name=question${questionNumber}]:checked`;
+    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+    // if answer is correct
+    if(userAnswer === currentQuestion.correctAnswer){
+      // add to the number of correct answers
+      numCorrect++;
+
+      // color the answers green
+      answerContainers[questionNumber].getElementsByClassName.color='lightgreen';
+      }
+      // if answer is wrong or blank
+      else{
+        // color the answers red
+        answerContainers[questionNumber].style.color='red';
+      }
+  });
+// show number of correct answers from total
+resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
 
 // display quiz
 buildQuiz();
@@ -74,15 +106,3 @@ function buildQuiz() {
   // combine output list into one string of HTML and put it on the page
   quizContainer.innerHTML = output.join("");
 }
-
-// // stored list of answer choices
-// const answers = [];
-
-// // for each available answer
-// for (letter in currenQuestion.answers){
-
-//     // HTML radion button
-//     answers.push(
-
-//     )
-// }
