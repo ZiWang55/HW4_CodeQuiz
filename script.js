2901a432710d9a0d19a3989781b45f7ac1cb6337
zi wang <ziwang55@gmail.com>
2020-10-18 22:59:04 -0500

    added output function to link answer into html output

--- script.js
+++ script.js
@@ -44,4 +44,35 @@ const myQuestions = [
     }
 ];
 
-function buildQuiz()
+function buildQuiz(){
+    // variable for storing HTML output
+    const output = [];
+
+    // for each question
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+
+            // variable for list of possible answers
+            const answers = [];
+
+            // HTML radio button
+            answers.push(
+                '<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                    ${letter} :
+                    ${currentQuestion.answers[letter]}
+                </label>'
+            );
+        }
+
+        // add this question and its answers to the output
+        output.push(
+            '<div class="question'> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join('')} </div>'
+        );
+        }
+    );
+
+    // combine output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
+}
