19b0c7f3c99f73526e190df876d42d4ecd128f39
zi wang <ziwang55@gmail.com>
2020-10-20 15:43:16 -0500

    sorted all files in right folders for a cleaner look, added start screen and button for html

---
+++ main/js/timer.js
@@ -0,0 +1,13 @@
+// TIMER
+
+let count = 15;
+let interval = setInterval(function () {
+  document.getElementById("count").innerHTML = count;
+  count--;
+  if (count === 0) {
+    clearInterval(interval);
+    document.getElementById("count").innerHTML = "Done";
+// notifying out of time
+    alert("You're out of time!");
+  }
+}, 1000);
