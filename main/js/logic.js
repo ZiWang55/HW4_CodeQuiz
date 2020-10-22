5efab983fc5729fa749bc43cbf924b5cd09fdbcf
Zi Wang <m@ms-macbook-air.lan>
2020-10-22 13:16:40 -0500

    changed html structure for efficiency

--- main/js/logic.js
+++ main/js/logic.js
@@ -121,37 +121,6 @@ const endscreen = document.getElementById("endscreen");
 endscreen.style.display = "none";
 queContainer.style.display = "none";
 // questions in arrays
-const myQuestions = [
-  {
-    question: "Who invented JavaScript?",
-    answers: {
-      a: "Douglas Crockford",
-      b: "Sheryl Sandberg",
-      c: "Brendan Eich",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "Which one of these is a JavaScript package manager?",
-    answers: {
-      a: "Node.js",
-      b: "TypeScript",
-      c: "npm",
-    },
-    correctAnswer: "c",
-  },
-  {
-    question: "Which tool can you use to ensure code quality?",
-    answers: {
-      a: "Angular",
-      b: "jQuery",
-      c: "RequireJS",
-      d: "ESLint",
-    },
-    correctAnswer: "d",
-  },
-];
-
 // start quiz function
 
 // display quiz
---
+++ main/js/question.js
@@ -0,0 +1,30 @@
+const myQuestions = [
+    {
+      question: "Who invented JavaScript?",
+      answers: {
+        a: "Douglas Crockford",
+        b: "Sheryl Sandberg",
+        c: "Brendan Eich",
+      },
+      correctAnswer: "c",
+    },
+    {
+      question: "Which one of these is a JavaScript package manager?",
+      answers: {
+        a: "Node.js",
+        b: "TypeScript",
+        c: "npm",
+      },
+      correctAnswer: "c",
+    },
+    {
+      question: "Which tool can you use to ensure code quality?",
+      answers: {
+        a: "Angular",
+        b: "jQuery",
+        c: "RequireJS",
+        d: "ESLint",
+      },
+      correctAnswer: "d",
+    },
+  ];
